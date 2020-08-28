b1ebfbb25e33016191debcd9dbacc18ff611e8ea
Roberto Cervantes <robert.hct@gmail.com>
2020-08-27 23:50:17 -0500

    Vista de testimoniales con formulario de por medio

--- server/routes/index.js
+++ server/routes/index.js
@@ -31,5 +31,11 @@ module.exports = function() {
       .catch(error => console.log(error))
   });
 
+  router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales', {
+      pagina: 'Testimoniales'
+    });
+  });
+
   return router;
 }
